a61cd82458a70fb9a5991363990049e1a84d337c
ElNath05 <ssidjl12@gmail.com>
2024-05-18 05:04:40 +0900

    enemy spawn 2

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -12,7 +12,7 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         e_SpawnTime += Time.deltaTime; //적 생성 시간을 잼
-        if(e_SpawnTime > 1f)   //생성시간이 되면 타이머를 초기화 하고 적 소환함수 실행
+        if(e_SpawnTime > 1.5f)   //생성시간이 되면 타이머를 초기화 하고 적 소환함수 실행
         {
             e_SpawnTime = 0;
             SpawnBasic();
@@ -45,9 +45,17 @@ public class EnemySpawner : MonoBehaviour
 
         //플레이어 기준으로 특정 거리내에서 랜덤한 위치에 적 생성
         Vector3 randPos = playerPos + new Vector3(randDir.x, randDir.y, 0) * randDist;
-        for(int i = 0; i < 6; i++) {
-            GameObject enemy =GameManager.Instance.pool.Get(2);
-            enemy.transform.position = randPos;
+        GameObject enemy1 = GameManager.Instance.pool.Get(2);
+        enemy1.transform.position = randPos;
+
+        //이후 생성될 적들이 첫 번째로 생성된 적 기준으로 어느정도 모아져서 생성되도록 함
+        for (int i = 0; i < 6; i++) {
+            GameObject enemy2 =GameManager.Instance.pool.Get(2);
+
+            Vector2 randDir2 = Random.insideUnitCircle.normalized;  //반복문을 돌 때마다 랜덤방향을 받아냄
+            //첫 번째로 생성된 적 기준 주변으로 최소한의 거리에 새로 생성된 적의 위치를 지정
+            Vector3 randPos2 = randPos + new Vector3(randDir2.x, randDir2.y, 0) * 0.6f;
+            enemy2.transform.position = randPos2;
         }
     }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public GameObject player;
     public PoolManager pool;
+
+    private float sec;  //게임 시간중 초를 세는 변수
+    private float min;  //게임 시간중 분을 세는 변수
+    public Text time;
     private void Awake()
     { if (Instance == null)
         {
@@ -26,6 +31,33 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        sec += Time.deltaTime;
+        if(sec > 59.5)
+        {
+            sec = 0;
+            min += 1;
+        }
+        if(min < 9.5)
+        {
+            if (sec < 9.5)
+            {
+                time.text = "0" + min.ToString("F0") + ":0" + sec.ToString("F0");
+            }
+            else
+            {
+                time.text = "0" + min.ToString("F0") + ":" + sec.ToString("F0");
+            }
+        }
+        else
+        {
+            if (sec < 9.5)
+            {
+                time.text = min.ToString("F0") + ":0" + sec.ToString("F0");
+            }
+            else
+            {
+                time.text = min.ToString("F0") + ":" + sec.ToString("F0");
+            }
+        }
     }
 }
