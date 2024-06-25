d2e54e786e8d27f799b655889cb178d6cdc69f20
ElNath05 <121789470+ElNath05@users.noreply.github.com>
2024-06-26 07:34:55 +0900

    last 3

--- Assets/Scripts/Scanner.cs
+++ Assets/Scripts/Scanner.cs
@@ -15,7 +15,7 @@ public class Scanner : MonoBehaviour
         //(레이캐스트의 시작위치, 범위, 캐스팅방향, 캐스팅 길이, 적의 레이어) 원형캐스트는 방향이 필요없기때문에 Vectro2.zero를 사용
         targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer);  //원형의 레이범위 내에 들어온 오브젝트들을 리스트에 넣음
         //targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer2);
-        nearTarget = GetNearest();
+        //nearTarget = GetNearest();
         randTarget = GetRandomTarget();
     }
 
@@ -31,24 +31,24 @@ public class Scanner : MonoBehaviour
         return result;
     }
 
-    Transform GetNearest()
-    {
-        Transform result = null;
-        float dist = 100;   //최소거리 비교용으로 사용할 변수
+    //Transform GetNearest()
+    //{
+    //    Transform result = null;
+    //    float dist = 100;   //최소거리 비교용으로 사용할 변수
 
-        //레이에 맞은 타겟중에서 플레이어와의 거리가 가장 가까운 오브젝트의 transform을 result로 저장
-        foreach (RaycastHit2D target in targets)
-        {
-            Vector3 myPos = transform.position;
-            Vector3 targetPos = target.transform.position;
-            float curDist = Vector3.Distance(myPos, targetPos); //Vector3.Distance로 플레이어와 타겟사이의 거리를 구함
+    //    //레이에 맞은 타겟중에서 플레이어와의 거리가 가장 가까운 오브젝트의 transform을 result로 저장
+    //    foreach (RaycastHit2D target in targets)
+    //    {
+    //        Vector3 myPos = transform.position;
+    //        Vector3 targetPos = target.transform.position;
+    //        float curDist = Vector3.Distance(myPos, targetPos); //Vector3.Distance로 플레이어와 타겟사이의 거리를 구함
 
-            if(curDist < dist)
-            {
-                dist = curDist;
-                result = target.transform;
-            }
-        }
-        return result;
-    }
+    //        if(curDist < dist)
+    //        {
+    //            dist = curDist;
+    //            result = target.transform;
+    //        }
+    //    }
+    //    return result;
+    //}
 }
